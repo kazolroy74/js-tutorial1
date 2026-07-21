d77a31b9aef0411cef9851ffb2a64ef1b926b1b1
kazolroy74 <kazolroy74@gmail.com>
2026-07-22 04:10:13 +0600

    js commit four

---
+++ length.js
@@ -0,0 +1,18 @@
+function func() {
+    //length property for string
+    console.log("w3school".length)
+}
+func();
+
+//checking if a string is empty
+let emptyString = '';
+console.log(emptyString.length === 0);
+
+//accessing last character of a string
+let str = 'javascript';
+const lastIndex = str.length - 1;
+console.log(lastIndex);
+
+let str2 = 'bangladesh';
+const newIndex = str2.length - 1;
+console.log(newIndex);
